98926a026206bb0121aecde9a243d63bb1887aa8
Munteanu Horia <horiamunteanu2000@yahoo.com>
2020-07-20 21:30:04 +0300

    Level commit

---
+++ src/main/java/ro/jademy/millionaire/Level.java
@@ -0,0 +1,11 @@
+package ro.jademy.millionaire;
+
+public class Level {
+    private int number;
+    private int reward;
+    private int difficultyLevel;
+
+    public Level(int number, int reward, int difficultyLevel){
+
+    }
+}
