9b9f2592fbed1a9e0b54e4adc93259642ed33734
Munteanu Horia <horiamunteanu2000@yahoo.com>
2020-07-20 21:25:45 +0300

    Game commit

--- src/main/java/ro/jademy/millionaire/Game.java
+++ src/main/java/ro/jademy/millionaire/Game.java
@@ -1,5 +1,21 @@
 package ro.jademy.millionaire;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Game {
-    
+
+    // 15 question
+    // 4 break points ->5, 10, 14, 15
+    // level [1-5]    difficulty 0
+    // level [6-10]   difficulty 1
+    // level [11-14]  difficulty 2
+    // level 15       difficulty 3
+
+    private List<Question> difficultyZeroQuestionList = new ArrayList<Question>();
+    private List<Question> difficultyOneQuestionList = new ArrayList<Question>();
+    private List<Question> difficultyTwoQuestionList= new ArrayList<Question>();
+    private List<Question> difficultyThreeQuestionList = new ArrayList<Question>();
+
+
 }
